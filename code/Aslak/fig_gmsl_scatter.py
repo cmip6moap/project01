954a7ebaf640ca118192f84ee9b007caca882a14
Aslak Grinsted <ag@glaciology.net>
2021-10-27 14:27:21 +0200

    compare to ar6 fig spm8

--- code/Aslak/fig_gmsl_scatter.py
+++ code/Aslak/fig_gmsl_scatter.py
@@ -9,7 +9,7 @@ import corner
 import hadcrut5
 import re
 import os
-from settings import scenariocolors, datafolder
+from settings import scenariocolors, baseline_period, datafolder
 from misc_tools import confidence_ellipse
 from plot_comparison_data import plot_comparison
 
@@ -42,6 +42,25 @@ for groupix, g in G:
         confidence_ellipse(g.Tavg_ice, g.GMSL*1000, facecolor=col, label=label)
     else:
         confidence_ellipse(g.Tavg_ice, g.GMSL*1000, facecolor=col)
+
+
+
+#ADD AR6
+Sar6 = pd.read_csv(f"{datafolder}/raw_data/AR6 Fig spm08ad/global_sea_level_projected.csv",index_col='Year')
+scenarios = ['SSP1-1.9', 'SSP1-2.6', 'SSP2-4.5', 'SSP3-7.0', 'SSP5-8.5']
+tasfile = f"{datafolder}/raw_data/AR6 Fig spm08ad/tas_global_Historical.csv"
+tas =pd.read_csv(tasfile)
+tasbase = tas[(tas.Year>=baseline_period[0]) & (tas.Year<=baseline_period[1])].mean()
+for scenario in scenarios:
+    tasfile = f"{datafolder}/raw_data/AR6 Fig spm08ad/tas_global_{scenario.replace('-','_').replace('.','_')}.csv"
+    tas =pd.read_csv(tasfile)
+    s = Sar6[f'{scenario} Central']
+    dSdt = (s[2100]-s[2050])/50
+    t = tas[(tas.Year>=2050) & (tas.Year<=2100)].mean() - tasbase
+    plt.plot(t['Mean'],dSdt*1000,'bo')
+    dSdt = (s[2050]-s[2020])/30
+    t = tas[(tas.Year>=2020) & (tas.Year<=2050)].mean() - tasbase
+    plt.plot(t['Mean'],dSdt*1000,'mo')
 
 
 plot_comparison('GMSL')
