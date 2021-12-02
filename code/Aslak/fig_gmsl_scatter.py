4fd2cda3eefac95a614797120e4f0d43dea03bc5
Aslak Grinsted <ag@glaciology.net>
2021-12-02 11:04:56 +0100

    Update fig_gmsl_scatter.py

--- code/Aslak/fig_gmsl_scatter.py
+++ code/Aslak/fig_gmsl_scatter.py
@@ -41,26 +41,27 @@ for groupix, g in G:
     if groupix[1] < 2040:
         confidence_ellipse(g.Tavg_ice, g.GMSL*1000, facecolor=col, label=label)
     else:
-        confidence_ellipse(g.Tavg_ice, g.GMSL*1000, facecolor=col)
+        confidence_ellipse(g.Tavg_ice, g.GMSL*1000, facecolor=col, linestyle = '--')
 
 
 
 #ADD AR6
-Sar6 = pd.read_csv(f"{datafolder}/raw_data/AR6 Fig spm08ad/global_sea_level_projected.csv",index_col='Year')
-scenarios = ['SSP1-1.9', 'SSP1-2.6', 'SSP2-4.5', 'SSP3-7.0', 'SSP5-8.5']
-tasfile = f"{datafolder}/raw_data/AR6 Fig spm08ad/tas_global_Historical.csv"
-tas =pd.read_csv(tasfile)
-tasbase = tas[(tas.Year>=baseline_period[0]) & (tas.Year<=baseline_period[1])].mean()
-for scenario in scenarios:
-    tasfile = f"{datafolder}/raw_data/AR6 Fig spm08ad/tas_global_{scenario.replace('-','_').replace('.','_')}.csv"
+if True:
+    Sar6 = pd.read_csv(f"{datafolder}/raw_data/AR6 Fig spm08ad/global_sea_level_projected.csv",index_col='Year')
+    scenarios = ['SSP1-1.9', 'SSP1-2.6', 'SSP2-4.5', 'SSP3-7.0', 'SSP5-8.5']
+    tasfile = f"{datafolder}/raw_data/AR6 Fig spm08ad/tas_global_Historical.csv"
     tas =pd.read_csv(tasfile)
-    s = Sar6[f'{scenario} Central']
-    dSdt = (s[2100]-s[2050])/50
-    t = tas[(tas.Year>=2050) & (tas.Year<=2100)].mean() - tasbase
-    plt.plot(t['Mean'],dSdt*1000,'bo')
-    dSdt = (s[2050]-s[2020])/30
-    t = tas[(tas.Year>=2020) & (tas.Year<=2050)].mean() - tasbase
-    plt.plot(t['Mean'],dSdt*1000,'mo')
+    tasbase = tas[(tas.Year>=baseline_period[0]) & (tas.Year<=baseline_period[1])].mean()
+    for scenario in scenarios:
+        tasfile = f"{datafolder}/raw_data/AR6 Fig spm08ad/tas_global_{scenario.replace('-','_').replace('.','_')}.csv"
+        tas =pd.read_csv(tasfile)
+        s = Sar6[f'{scenario} Central']
+        dSdt = (s[2100]-s[2050])/50
+        t = tas[(tas.Year>=2050) & (tas.Year<=2100)].mean() - tasbase
+        plt.plot(t['Mean'],dSdt*1000,'bo')
+        dSdt = (s[2050]-s[2020])/30
+        t = tas[(tas.Year>=2020) & (tas.Year<=2050)].mean() - tasbase
+        plt.plot(t['Mean'],dSdt*1000,'mo')
 
 
 plot_comparison('GMSL')
